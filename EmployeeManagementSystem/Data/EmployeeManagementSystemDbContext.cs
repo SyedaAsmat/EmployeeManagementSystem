dceff13aa6f6f1a1337d077d1b3746be40edcc9b
HP <HP@DESKTOP-AQ07QGH>
2024-02-09 16:38:37 +0530

    Add project files.

---
+++ EmployeeManagementSystem/Data/EmployeeManagementSystemDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Project_EmployeeManangementSystem.Models;
+
+namespace EmployeeManagementSystem.Data
+{
+    public class EmployeeManagementSystemDbContext : DbContext
+    {
+        public EmployeeManagementSystemDbContext (DbContextOptions<EmployeeManagementSystemDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project_EmployeeManangementSystem.Models.EmpProfile> EmpProfile { get; set; } = default!;
+    }
+}
---
+++ EmployeeManagementSystem/Migrations/20240209101642_ToDbMigrate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagementSystem.Migrations
+{
+    public partial class ToDbMigrate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmpProfile",
+                columns: table => new
+                {
+                    EmpCode = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeptCode = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmpProfile", x => x.EmpCode);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmpProfile");
+        }
+    }
+}
